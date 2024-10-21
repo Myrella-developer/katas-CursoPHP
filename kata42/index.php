3c443a6849e0d2b5d95fa845fd373414e8049c06
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-10-21 13:23:32 +0200

    ajustes kata 42

--- kata42/index.php
+++ kata42/index.php
@@ -1,41 +1,31 @@
 
-
 <?php
+define('FRAME_CHAR', '#');
+define('BORDER_PADDING', 4);
+$texto = readline("Ingrese el texto aqui");
+printFrame($texto);
 
 function printFrame(string $texto): void {
-
-    //divide el texto en palabras
-
     $quotes = explode(" ", $texto);
-
-    //encuentra la longitud de la palabra más larga
-
     $maxLength = 0;
+
     foreach ($quotes as $quote) {
         if(strlen($quote) > $maxLength) {
             $maxLength = strlen($quote);
         }
     }
+    $frame = str_repeat(FRAME_CHAR, $maxLength + BORDER_PADDING);
+    echo $frame . PHP_EOL;
 
-    //imprime la parte superior del marco 
-    $frame = str_repeat("🧚‍♀️", $maxLength + 4);
-    echo $frame . "<br>";
-
-    //imprimie cada palabra en una linea con almohadillas
     foreach ($quotes as $quote) {
-    // primero calculams los espacios a la izquierda y a la derecha 
-        $leftPadding = floor(($maxLength - strlen($quote)) / 2); //lo dividimos por 2 para que los espacios sean equitativos en ambos lados
+        $leftPadding = floor(($maxLength - strlen($quote)) / 2);
         $rightPadding = $maxLength - strlen($quote) - $leftPadding;
-        $paddedWord = str_repeat("  ", $leftPadding) . $quote . str_repeat("   ", $rightPadding);
-        echo "🧚‍♀️".$paddedWord. "🧚‍♀️<br>";
+        $paddedWord = str_repeat(" ", $leftPadding) . $quote . str_repeat(" ", $rightPadding);
+        echo FRAME_CHAR . $paddedWord . FRAME_CHAR . PHP_EOL;
     }
 
-    //imprimi la parte inferior del marco
-    echo $frame;
+    echo $frame.PHP_EOL;
 }
-
-$texto = "¿Has Debugado hoy?";
-printFrame($texto);
 
 
 ?>

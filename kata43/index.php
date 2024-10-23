f289619f5709084e60f17cca919f2ae109efaf4e
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-10-23 12:01:12 +0200

    plan vacacionales ejecutable

--- kata43/index.php
+++ kata43/index.php
@@ -6,10 +6,12 @@ require_once 'webPlanes.php';
 
 $plan1 = new PlanVac('Marbella', '2024-06-06', 'Escapada', tipoVac::Playa);
 $plan2 = new PlanVac('Madrid', '2024-07-06', 'Tarde de Teatro', tipoVac::Ciudad);
+$plan3 = new PlanVac('Barcelona','2024-12-24', 'Cena Familia de Navidad', tipoVac::Campo);
+$plan4 = new PlanVac('Havaii','2025-01-05', 'Viaje en Familia', tipoVac::Playa);
+$plan5 = new PlanVac('Rio de Janeiro','2025-03-10', 'Carnaval Rio', tipoVac::Ciudad);
 
-
-$planVac = new WebPlanes('Planes de Junio');
-$planVac->planificador = [$plan1, $plan2];
+$planVac = new WebPlanes('Planes de Vacaciones');
+$planVac->planificador = [$plan1, $plan2, $plan3, $plan4, $plan5];
 
 $planVac->Mostrar();
 
--- kata43/webPlanes.php
+++ kata43/webPlanes.php
@@ -14,17 +14,39 @@
     
         public function Mostrar() {
             $hoy = new DateTime();
+            $actividadesPendientes = [];
         
             foreach ($this->planificador as $plan) {
                 $fechaPlan = DateTime::createFromFormat('Y-m-d', $plan->fecha);
         
                 if ($fechaPlan < $hoy) {
-                   echo "Actividades ya Realizadas: <br>".$plan-> __toString()."<br>";
-                } 
+                   echo "Actividades ya Realizadas: ".$plan-> __toString().PHP_EOL;
+                } else {    
+                    echo PHP_EOL."Actividade Pendiente: ".$plan-> __toString().PHP_EOL;
+                    echo PHP_EOL."Deseas Anular actividad?".PHP_EOL;
+                    echo "1. Si".PHP_EOL;       
+                    echo "2. No".PHP_EOL;
+                    if (readline() ==1){
+                        $this->AnularPlan($plan);
+                    }else{
+                        echo "Actividad no anulada".PHP_EOL;
+                        $actividadesPendientes[] = $plan;
+                      
+                    }                
+                }
+            } 
 
+            echo PHP_EOL."Actividades pendientes actualizadas: ".$plan->__toString().PHP_EOL;
+            foreach ($actividadesPendientes as $actividad) {
+                echo $actividad->__toString().PHP_EOL;
             }
-    
-         
         }
-    }
+        
+        public function AnularPlan(PlanVac $plan) {
+            $plan->fecha = "0000-00-00";
+            echo "Actividad Anulada".PHP_EOL;
+        }
+           
+        }
+    
 ?>

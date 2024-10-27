9c6bf9f98e3880a9a0a8d7a5dbb1623b9feb23a0
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-10-27 18:44:54 +0100

    correccion kata 42

--- kata42/index.php
+++ kata42/index.php
@@ -1,31 +1,45 @@
-
 <?php
+
 define('FRAME_CHAR', '#');
 define('BORDER_PADDING', 4);
-$texto = readline("Ingrese el texto aqui");
+
+$texto = readline("Introduce el texto a imprimir: ");
 printFrame($texto);
 
 function printFrame(string $texto): void {
     $quotes = explode(" ", $texto);
-    $maxLength = 0;
-
-    foreach ($quotes as $quote) {
-        if(strlen($quote) > $maxLength) {
-            $maxLength = strlen($quote);
-        }
-    }
-    $frame = str_repeat(FRAME_CHAR, $maxLength + BORDER_PADDING);
-    echo $frame . PHP_EOL;
-
-    foreach ($quotes as $quote) {
-        $leftPadding = floor(($maxLength - strlen($quote)) / 2);
-        $rightPadding = $maxLength - strlen($quote) - $leftPadding;
-        $paddedWord = str_repeat(" ", $leftPadding) . $quote . str_repeat(" ", $rightPadding);
-        echo FRAME_CHAR . $paddedWord . FRAME_CHAR . PHP_EOL;
-    }
-
-    echo $frame.PHP_EOL;
+    $maxLength = getLongestWordLength($quotes);
+    printFrameRow($maxLength);
+    printWordRows($quotes, $maxLength);
+    printFrameRow($maxLength);
 }
 
+function getLongestWordLength(array $quotes): int {
+    $size = 0;
+    foreach ($quotes as $quote) {
+        if (mb_strlen($quote) > $size) {
+            $size = mb_strlen($quote);
+        }
+    }
+    return $size;
+}
 
-?>
+function printFrameRow(int $n_columns): void {
+    for ($i = 0; $i < $n_columns + BORDER_PADDING; $i++) {
+        echo FRAME_CHAR;
+    }
+    echo PHP_EOL;
+}
+
+function printWordRows(array $quotes, int $maxLength): void {
+    foreach ($quotes as $quote) {
+        echo FRAME_CHAR . " " . $quote;
+        printRemainingRow(mb_strlen($quote), $maxLength);
+    }
+}
+
+function printRemainingRow(int $current_max_length, int $maxLength): void {
+    $columns_diff = $maxLength - $current_max_length;
+    echo str_repeat(" ", $columns_diff) . " " . FRAME_CHAR . PHP_EOL;
+}
+
